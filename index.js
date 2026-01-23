f553a09715b0eaaa6ddc79aa9aac1054e2251e5a
Daniel Tsav <danieltsav@Daniels-MacBook-Pro.local>
2026-01-23 17:04:18 +0000

    Allow localhost:3001 for CORS

--- index.js
+++ index.js
@@ -130,10 +130,12 @@ const allowedOrigins = (
         process.env.UI_ORIGIN,
         'capacitor://localhost',
         'http://localhost:3000',
+        'http://localhost:3001',
         'http://127.0.0.1:3000',
       ]
     : [
         'http://localhost:3000',
+        'http://localhost:3001',
         'http://localhost:3002',
         'http://127.0.0.1:3000',
         'https://pos.savopay.co',
